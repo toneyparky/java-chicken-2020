5be561bd16cb43db47e6965a8bec77bf0d992243
toneyparky <qkrtmddhks95@gmail.com>
2020-04-10 13:48:27 +0900

    feat: Orders 구현 및 테스트 작성
    Table Orders를 멤버 변수로 가지게 함

---
+++ src/main/java/domain/Orders.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.List;
+
+public class Orders {
+	private List<Order> orders;
+
+	public Orders() {
+	}
+
+	public Orders(List<Order> orders) {
+		this.orders = orders;
+	}
+}
---
+++ src/test/java/domain/OrdersTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrdersTest {
+
+	@DisplayName("Orders 정상 생성 테스트")
+	@Test
+	void create() {
+		List<Order> orders = Arrays.asList(
+				new Order(MenuRepository.menus().get(0), new Amount(10)),
+				new Order(MenuRepository.menus().get(2), new Amount(10))
+		);
+		assertThat(new Orders(orders)).isInstanceOf(Orders.class);
+	}
+}
